03885f069faea975f6158250e5e8da4f319ab8e4
adrien.denis <adrien.denis@uclouvain.be>
2017-10-11 13:38:33 +0200

    Commit of chapter 2 Xaml Essential.

---
+++ XAML-Essential/Exercice1/Exercice1/QuotesPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exercice1
+{
+    public partial class QuotesPage : ContentPage
+    {
+        private string[] quotes;
+        private int quotesIndice;
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+            quotes = new string[] {
+                "Phrase1",
+                "Phrase2",
+                "Phrase3"
+            };
+            quotesIndice = 0;
+            SetQuote();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            quotesIndice++;
+
+            if (quotesIndice > quotes.Length - 1)
+                quotesIndice = 0;
+
+            SetQuote();
+        }
+
+        private void SetQuote()
+        {
+            QuoteLabel.Text = quotes[quotesIndice];
+        }
+    }
+}
